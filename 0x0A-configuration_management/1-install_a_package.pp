1165fbd2b8a7fab2656991d27a5a20892e151597
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-23 17:38:50 +0300

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-# flask from pip3 Install flask, Version must be 2.1.0
-exec { 'Flask':
-  command => '/usr/bin/apt-get -y install Flask -v 2.1.0',
+# flask from pip3 2.1.0
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
