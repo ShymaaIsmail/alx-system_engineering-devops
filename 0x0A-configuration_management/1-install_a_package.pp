bea1f8edc6c97db22edd124f8ddb6ddbd81a6ee5
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-23 17:27:07 +0300

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-#!/usr/bin/pup
-# Using Puppet, install flask from pip3 Install flask, Version must be 2.1.0
+# install flask from pip3 Install flask, Version must be 2.1.0
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
