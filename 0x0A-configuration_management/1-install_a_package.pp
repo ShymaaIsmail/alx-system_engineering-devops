60134c9a4f23ba881a0612b80259ab0587c38e5c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-23 17:40:32 +0300

    fix 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # flask from pip3 2.1.0
+package { 'python3-pip':
+  ensure => installed,
+}
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip']
 }
