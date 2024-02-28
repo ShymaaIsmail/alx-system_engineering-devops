7aedbd5e2fcfe6ea4da5f5fa8c674924ffd1d6f3
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-28 09:27:31 +0300

    100 fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # mix puppet with ssh cnfig
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
+  match  => '^#?\s*PasswordAuthentication\s',
   ensure => present,
 }
 
 file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
+  match  => '^#?\s*IdentityFile\s',
   ensure => present,
 }
