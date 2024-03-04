7777830b533c26a4bdb0bd3ed0941f779b4cc3fc
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-04 12:31:56 +0300

    nginx_hostname

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -29,7 +29,7 @@ server {
     server_name _;
 
     location / {
-        add_header  X-Served-By $nginx_hostname;
+        add_header  X-Served-By ${nginx_hostname};
         try_files \$uri \$uri/ =404;
     }
 
