9098131b2508d3a52b2f475ab09ce7eb6581d41f
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-04 12:27:15 +0300

    <%= @hostname %>

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -25,7 +25,7 @@ server {
     server_name _;
 
     location / {
-        add_header  X-Served-By $hostname;
+        add_header  X-Served-By <%= @hostname %>;
         try_files \$uri \$uri/ =404;
     }
 
