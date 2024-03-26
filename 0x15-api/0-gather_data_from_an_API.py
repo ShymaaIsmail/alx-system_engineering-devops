bb44465da0dc2266c0a526a3b2ea2795fce2c999
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-26 13:40:18 +0300

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,16 +6,19 @@ import sys
 
 def get_employee_todo_progress(employee_id):
     """get_employee_todo_progress"""
+    user_api = "https://jsonplaceholder.typicode.com/users/" + str(employee_id)
     url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     response = requests.get(url)
-    if response.status_code == 200:
+    user_details = requests.get(user_api)
+    if response.status_code == 200 and user_details.status_code == 200:
         todos = response.json()
-        employee_name = todos[0]['userId']
+        user = user_details.json()
+        employee_name = user['name']
         done_tasks = [todo['title'] for todo in todos if todo['completed']]
         total_tasks = len(todos)
         number_of_done_tasks = len(done_tasks)
-        print(f"Employee {employee_name} is done with tasks \
-                ({number_of_done_tasks}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks"
+              f"({number_of_done_tasks}/{total_tasks}):")
         for task in done_tasks:
             print(f"\t {task}")
     else:
