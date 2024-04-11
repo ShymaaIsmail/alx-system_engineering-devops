bf4211bf28aea7460863edd1bd398d4c659e7a28
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-11 17:39:14 +0300

    sdws

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,7 @@ def number_of_subscribers(subreddit):
     """number of subscribers"""
     try:
         url = "https://www.reddit.com/r/{0}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-                   "AppleWebKit/537.36 (KHTML, like Gecko) " +
-                   "Chrome/58.0.3029.110 Safari/537.3"}
+        headers = {'User-Agent': 'by u/qasqot79'}
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = json.loads(response.content.decode('utf-8'))
