27bdd558246061282cdb6aa1c1d5dbff085e0017
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-04-11 04:04:00 +0300

    Update 2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+2-recurse
+"""
+
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """recurse"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    params = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        if after:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    else:
+        return None
