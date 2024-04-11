07f0541b9141616372d72d512f50672ee521c93b
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-11 12:50:51 +0300

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers for
-a given subreddit.
-If the subreddit is invalid, it returns 0.
+0-subs
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'by u/qasqot79'}
+    """number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
-
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
