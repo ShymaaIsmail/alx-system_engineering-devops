7fa24ab3529b041c6567b4748ed9e89305c762b9
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-11 16:39:52 +0300

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-               "AppleWebKit/537.36 (KHTML, like Gecko) " +
-               "Chrome/58.0.3029.110 Safari/537.3"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    try:
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                   "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                   "Chrome/58.0.3029.110 Safari/537.3"}
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.exceptions.RequestException as e:
         return 0
