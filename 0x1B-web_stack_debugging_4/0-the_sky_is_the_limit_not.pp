c6fbcb0d36a1bb45e40e05b3b21882d5b8e6d9b6
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-17 10:57:16 +0300

    puppet lint

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,17 @@
+#update limit of etc default nginx 
+# Define the ULIMIT value
+$ulimit_value = '100000'
+# Generate the content for the nginx default configuration file
+$file_content = "
+# Additional options to pass to nginx
+ULIMIT=\"-n ${ulimit_value}\"
+"
+
+# Ensure the file /etc/default/nginx exists and has specific content
+file { '/etc/default/nginx':
+  ensure  => file,
+  content => $file_content,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
